76ccc6e2e4794e682ab04a079b659f28e7cb2271
luciagore <lucy.gore@hotmail.co.uk>
2018-07-30 11:04:09 +0100

    updated tests and midiStore.js

--- spec/midi_spec.js
+++ spec/midi_spec.js
@@ -2,29 +2,42 @@
 
 describe('MidiStore',function(){
 	var midistore;
-	var mididata = {0: { note:1, veloctiy:2, delay:2 },
-									1: { note:1, veloctiy:2, delay:2 },
-									2: { note:1, veloctiy:2, delay:2 },
-									3: { note:1, veloctiy:2, delay:2 }
+	var mididata = {0: { note:1, delay:2 },
+									1: { note:1, delay:2 },
+									2: { note:1, delay:2 },
+									3: { note:1, delay:2 }
 								}
 	//firebase.database().ref().child("midistore").push(mididata)
-	
+
+	var push_double = {push: function(midi_ids){return {key: 'SOME_RANDOM_FB_KEY'}},
+                      on: function(value){},
+                      set: function(value){},
+                      child: function(name){return {set: function(value){listofUsers_mock.push(1)}}}
+
+                    };
+  var fb_ref_double = {ref: function(MidiStore){return push_double}};
+
 	beforeEach(function(){
-		midistore = new MidiStore("instrument", mididata);
+		midistore = new MidiStore("piano", 'lucy', mididata, fb_ref_double);
 	});
   it('has an ID', function(){
-    expect(midistore.getID()).toEqual(0)
+    expect(midistore.getKey()).toEqual("SOME_RANDOM_FB_KEY")
   })
 
   it('stores an instrument', function(){
-    expect(midistore.instrumentID()).toEqual("instrument")
+    expect(midistore.getInstrument()).toEqual("piano")
   })
 
   it('stores a set of notes', function(){
-    expect(midistore.mididata()).toEqual(mididata)
+    expect(midistore.getMididata()).toEqual(mididata)
   })
 
   it('holds a user', function(){
     expect(midistore.getUser()).toEqual('lucy')
   })
+
+	xit('updates midi', function(){
+		midistore.updateMidi(new_mididata);
+		expect(midistore.getMididata()).toEqual(new_mididata)
+	})
 });
